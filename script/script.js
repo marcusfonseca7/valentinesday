be9b5a0531ffa005c9dc9c27c75a72e504ab9fad
Marcus <marcus.fonseca968@gmail.com>
2026-06-05 18:24:42 -0300

    feat: seção hero parcialmente finalizada

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const menuOptionLetter = document.getElementById("1");
+const textOptionLetter = document.getElementById("text-menu");
+
+function borderColor() {
+  menuOptionLetter.style.borderBottom = "2px solid var(--vermelho-principal)";
+  textOptionLetter.style.color = "var(--vermelho-principal)";
+}
