2db96a15ca35869f7eba5d98bfaf3fc611d387a7
Marcus <marcus.fonseca968@gmail.com>
2026-06-09 14:59:54 -0300

    feat: contador finalizado

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 const menuOptionLetter = document.getElementById("1");
 const textOptionLetter = document.getElementById("text-menu");
 
-const years = document.getElementById("years")
-const months = document.getElementById("months")
-const days = document.getElementById("days")
-const hours = document.getElementById("hours")
-const minutes = document.getElementById("minutes")
-const seconds = document.getElementById("seconds")
+const yearsDiv = document.getElementById("years")
+const monthsDiv = document.getElementById("months")
+const daysDiv = document.getElementById("days")
+const hoursDiv = document.getElementById("hours")
+const minutesDiv = document.getElementById("minutes")
+const secondsDiv = document.getElementById("seconds")
 
 function borderColor() {
   menuOptionLetter.style.borderBottom = "2px solid var(--vermelho-principal)";
@@ -14,22 +14,47 @@ function borderColor() {
 }
 
 function timeTogether() {
-  dataInicial = new Date(1701894601 * 1000)
+  inicialDate = new Date(1701894601 * 1000)
 
 
-  const dataAtual = new Date()
+  const dateNow = new Date()
 
-  differenceDate = dataAtual - dataInicial
+  differenceDate = dateNow - inicialDate
 
   let seconds = Math.floor(differenceDate / 1000)
   let minutes = Math.floor(differenceDate / (1000 * 60))
   let hours = Math.floor(differenceDate / (1000 * 60 * 60))
-  let days = Math.floor(differenceDate / (1000 * 60 * 60 * 24))
-  console.log(differenceDate)
-  console.log(seconds)
-  console.log(minutes)
-  console.log(hours)
-  console.log(days)
+  
+  let days = dateNow.getDate() - inicialDate.getDate()
+  let months = dateNow.getMonth() - inicialDate.getMonth()
+  let years = dateNow.getFullYear() - inicialDate.getFullYear()
+
+  if (days < 0) {
+        months--;
+        const lastMonth = new Date(
+            dateNow.getFullYear(),
+            dateNow.getMonth(),
+            0
+        ).getDate();
+
+        days += lastMonth;
+    }
+
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+
+  secondsDiv.textContent = `${seconds % 60}`
+  minutesDiv.textContent = `${minutes % 60}`
+  hoursDiv.textContent = `${hours % 24}`
+  daysDiv.textContent = `${days}`
+  monthsDiv.textContent = `${months}`
+  yearsDiv.textContent = `${years}`
+
+
 }
 
 timeTogether()
+setInterval(timeTogether, 1000)
